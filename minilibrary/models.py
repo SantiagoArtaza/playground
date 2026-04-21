5f7082dc8dc9609cbfebd2e476b17b9978595d4a
SantiagoArtaza <s.ariel.out@gmail.com>
2026-04-21 18:52:19 -0300

    Add minilibrarydatabase

--- minilibrary/models.py
+++ minilibrary/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    birth_date = models.DateField(null=True, blank=True)
+    def __str__(self):
+        return self.name
+
+class Book(models.Model):
+    title=models.CharField(max_length=200)
+    published_date=models.DateField(null=True, blank=True)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books') # related_name allows us to access the books of an author using author.books.all
+    pages = models.IntegerField(null=True, blank=True)
+    isbn = models.CharField(max_length=20, unique=True)
+    def __str__(self):
+        return self.title
